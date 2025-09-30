34fcf7d3ad79486acddf434673fb1043aeeeb2fe
Shounak.cs <shounakkumbhakar650@gmail.com>
2025-09-30 21:37:44 +0530

    second commit

--- script.js
+++ script.js
@@ -1,25 +1,33 @@
-const todoInput = document.getElementById("todo-input");
-const addTaskBtn = document.getElementById("add-task-btn");
-const todoList = document.getElementById("todo-list");
+document.addEventListener('DOMContentLoaded', () => {
+    const todoInput = document.getElementById("todo-input");
+    const addTaskBtn = document.getElementById("add-task-btn");
+    const todoList = document.getElementById("todo-list");
 
-let tasks = []
+    let tasks = JSON.parse(localStorage.getItem("tasks")) || []
 
-addTaskBtn.addEventListener('click', () => {
-    const taskText = todoInput.value.trim()
-    if (taskText === "") return;
+    tasks.forEach(task => renderTask(task))
+
+    addTaskBtn.addEventListener('click', () => {
+        const taskText = todoInput.value.trim()
+        if (taskText === "") return;
 
 
-    const newTask = {
-        id: Date.now(),
-        text: taskText,
-        completed: false
+        const newTask = {
+            id: Date.now(),
+            text: taskText,
+            completed: false
+        }
+
+        tasks.push(newTask)
+        saveTasks();
+        todoInput.value = "" //clear the input
+    })
+
+    function renderTask() {
+        console.log(task);
     }
-
-    tasks.push(newTask)
-    saveTasks();
-    todoInput.value = "" //clear the input
+    
+    function saveTasks() {
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+    }
 })
-
-function saveTasks() {
-    localStorage.setItem("tasks",JSON.stringify(tasks));
-}
